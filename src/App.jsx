98a55cce976579710bb1095c1c55acd5a8a21721
HanyMohamed <hsaad9231@gmail.com>
2024-06-20 14:34:12 +0300

    installation and start project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+function App() {
+
+  return (
+    <Container>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Hello, Material-UI with Vite!
+      </Typography>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+    </Container>
+
+  )
+}
+
+export default App
