1d8499b0b379e241d8225be9de9e1f176b11ff9f
HanyMohamed <hsaad9231@gmail.com>
2024-06-20 16:17:09 +0300

    How to use Button and Container

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,10 +1,10 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Container, Typography } from "@mui/material";
 
 
 export default function Notes() {
   return (
 
-    <>
+    <Container>
 
       <Typography
         variant="h6"
@@ -12,8 +12,21 @@ export default function Notes() {
         color="primary"
         align="center"
       >
-        h1. Heading
+        Learn Material-Ui
       </Typography>
-    </>
+      <Button variant="contained" color="primary" size="medium">Click here</Button>
+      <ButtonGroup color="secondary" variant="outlined" size="medium">
+        <Button type="submit"
+          onClick={() => console.log("you click one")}
+        >one</Button>
+        <Button type="submit"
+          onClick={() => console.log("you click two")}
+        >Two</Button>
+        <Button type="submit"
+          onClick={() => console.log("you click three")}
+        >three</Button>
+
+      </ButtonGroup>
+    </Container>
   )
 }
