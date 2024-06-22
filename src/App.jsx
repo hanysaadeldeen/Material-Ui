e31ae30eb8fb0e253f013d6632d6e0e413f0ecf0
HanyMohamed <hsaad9231@gmail.com>
2024-06-22 18:58:45 +0300

    custom Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Notes from "./pages/Notes";
 import Create from "./pages/Create";
+import { ThemeProvider, createMuiTheme } from "@mui/material";
+import { purple } from "@mui/material/colors";
+// import { ThemeProvider, createMuiTheme } from "@emotion/react";
 
+
+
+const Theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#f6f6f6"
+    },
+    secondary: purple
+  },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+  }
+})
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Notes />} exact path="/" />
-        <Route element={<Create />} path="/create" />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={Theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Notes />} exact path="/" />
+          <Route element={<Create />} path="/create" />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
