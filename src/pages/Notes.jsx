3f185bf66b22a5c3ec1f7e10c108c0fa1c5e641f
HanyMohamed <hsaad9231@gmail.com>
2024-06-23 14:07:56 +0300

    custom text field

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,9 +1,53 @@
-import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import { Button, ButtonGroup, Container, TextField, Typography, } from "@mui/material";
 
 import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import { useState } from "react";
+import { Tune } from "@mui/icons-material";
+
+
+// import makeStyles from '@mui/styles/makeStyles';
+
+// const useStyles = makeStyles({
+//   field: {
+//     marginTop: 20,
+//     marginBottom: 20,
+//     display: 'block'
+//   }
+// })
+
+
+
+
+
 export default function Notes() {
+
+  const [title, setTitle] = useState("")
+  const [desc, setDesc] = useState("")
+  const [titleError, setTitleError] = useState(false)
+  const [descError, setDescError] = useState(false)
+  // const Clases = useStyles()
+
+
+  const getData = (e) => {
+    e.preventDefault()
+    setTitleError(false)
+    setDescError(false)
+    if (title !== "" && desc !== "") {
+      console.log("title : ", title, "desc : ", desc);
+      setTitleError(false)
+      setDescError(false)
+    } else {
+      if (title === "" && desc === "") {
+        setTitleError(true)
+        setDescError(true)
+      }
+      title === "" ? setTitleError(Tune) : setTitleError(false)
+      desc === "" ? setDescError(Tune) : setDescError(false)
+
+    }
+  }
   return (
 
     <Container>
@@ -41,6 +85,40 @@ export default function Notes() {
       </Button>
       <HomeOutlinedIcon color="secondary" fontSize="large" />
       <HomeOutlinedIcon color="primary" fontSize="smalll" />
+
+      <form noValidate autoComplete="off" onSubmit={getData}>
+
+        <TextField id="outlined-basic"
+          onChange={(e) => setTitle(e.target.value)}
+          // className={Clases.field}
+          color="secondary"
+          fullWidth
+          required
+          multiline
+          rows={4}
+          size="small"
+          label="Outlined"
+          variant="outlined"
+          error={titleError}
+        />
+
+        <TextField id="filled-basic"
+          onChange={(e) => setDesc(e.target.value)}
+          color="secondary"
+          fullWidth
+          required
+          size="small"
+          label="Filled"
+          variant="filled"
+          error={descError}
+        />
+        <Button
+          type="submit"
+          color="secondary"
+          variant="contained" endIcon={<SendIcon />}>
+          Send
+        </Button>
+      </form>
     </Container>
   )
 }
