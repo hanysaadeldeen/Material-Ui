54d7006eab547b280199a43921828465aa4f1283
HanyMohamed <hsaad9231@gmail.com>
2024-06-24 12:45:12 +0300

    Add Grid To Content

--- src/pages/Notess.jsx
+++ src/pages/Notess.jsx
@@ -1,3 +1,4 @@
+import { Container, Grid, Paper } from "@mui/material"
 import { useEffect, useState } from "react"
 
 
@@ -9,16 +10,35 @@ const Notess = () => {
         fetch("http://localhost:8000/notes").then((response) => response.json()).then(data => setAllNotes(data))
     }, [])
     return (
-        <div>
+        <Container>
+            {/* <Grid container>
+                <Grid item md="4" lg="5">1</Grid>
+                <Grid item md="4" lg="5">2</Grid>
+                <Grid item md="4" lg="2">3</Grid>
+                <Grid item md="4" lg="2">4</Grid>
+            </Grid> */}
+
+            {/* <></> */}
+
             <h1>All notes</h1>
-            {allnotes && allnotes.map(ele => {
-                return (
-                    <div key={ele.id}>
-                        <h3>{ele.title}</h3>
-                    </div>
-                )
-            })}
-        </div>
+            <Grid container>
+
+
+                {allnotes && allnotes.map(ele => {
+                    return (
+                        <Grid key={ele.id} item sm={12} md={6} lg={4}>
+                            <div >
+                                <Paper>
+                                    <h3>{ele.title}</h3>
+                                    <p>{ele.details}</p>
+                                    <p>{ele.category}</p>
+                                </Paper>
+                            </div>
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        </Container>
     )
 }
 
